37624c2fef1588b2d66cc251f4a6e705aa3a01a6
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-12-02 11:17:25 +0300

    feat(Create a heading in the root app from main controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  
+
 #Get/about
   get "about", to: "about#index"
+  #Get/root
+  get "/", to: "main#index"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
